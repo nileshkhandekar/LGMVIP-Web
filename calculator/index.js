22b750ba84d4dbef2c531ebae81582a0f1527f4a
nileshkhandekar <nileshkhandekar2002@gmail.com>
2022-08-30 17:38:31 +0530

    .

---
+++ calculator/index.js
@@ -0,0 +1,44 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let scValue =''
+let ans =''
+
+for(item of buttons)
+{
+	item.addEventListener('click', (e) => {
+		buttonText = e.target.innerText;
+		console.log('button text is', buttonText);
+
+		if(buttonText == 'X')
+		{
+			buttonText = '*';
+			scValue += buttonText;
+			screen.value = scValue;
+		}
+		else if(buttonText == 'C')
+		{
+			scValue =  "";
+			screen.value = scValue;
+
+		}
+		else if(buttonText == '=')
+		{
+			try{
+				ans = eval(scValue);	
+				screen.value = ans;
+				scValue = ans;	
+			}	
+			catch{
+				screen.value = "Error!";
+				scValue = ""
+			}
+
+
+		}
+		else
+		{
+			scValue += buttonText;
+			screen.value = scValue;
+		}
+	})
+}
